21b686b7550b507d3356e6d700d7dd9511700297
pirimiri <linhdo@umass.edu>
2016-06-12 17:40:22 +0700

    Update and rename 30day.java to io.java

--- io.java
+++ io.java
@@ -1,16 +1,30 @@
-Input Format
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in); // use the Scanner class to read from stdin
+      String inputString = scan.nextLine(); // read a line of input and save it to a variable
+      scan.close(); // close the scanner
+      
+      // Your first line of output goes here
+      System.out.println("Hello, World.");
+      
+      System.out.println(inputString);
+   }
+}
+public class Solution {
+Input Format
 The first line contains an integer, . 
 The second line contains a double, . 
 The third line contains a string, .
 
 Output Format
-
 Print the sum of both integers on the first line, the sum of both doubles on the second line, 
 and then the two concatenated strings on the third line.
-
-import java.util.*;
-public class Solution {
    
    public static void main(String[] args) {
         int i = 4;
@@ -24,8 +38,8 @@ public class Solution {
         String s2;
         
         /* Read and save an integer, double, and String to your variables.*/
-        i2 = scan.nextInt();
-        d2 = scan.nextDouble();
+        i2 = scan.nextInt(); // Scans the next token of the input as an int.
+        d2 = scan.nextDouble(); // Scans the next token of the input as a double.
         scan.nextLine(); // read the rest of the line of input (newline character after the double token).
         s2 = scan.nextLine();
        
@@ -42,3 +56,4 @@ public class Solution {
         scan.close();
    }
 }
+
