7342b3d981c9be10ab3055db042f08af6ffd2414
pirimiri <linhdo@umass.edu>
2016-06-15 14:08:32 +0700

    Update logic.java

--- logic.java
+++ logic.java
@@ -1,12 +1,15 @@
 import java.util.*;
-
-public class Solution {
-   
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt(); 
-        scan.close();
-
-        System.out.println( ((n & 1) == 1 || (6 <= n && n <= 20)) ? "Weird" : "Not Weird");
-    }
+public class Weird {
+// If n is odd, print Weird
+// If n is even and in the inclusive range of 2 to 5, print Not Weird
+// If n is even and in the inclusive range of 6 to 20, print Weird
+// If n is even and greater than 20, print Not Weird
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt(); 
+      sc.close();
+      
+      // Weird: odd or (even) 6...20; Not weird: otherwise
+      System.out.println( ((n % 2) == 1 || (6 <= n && n <= 20)) ? "Weird" : "Not Weird");
+   }
 }
