d4333794bc824e9bb632b570b4c70f2746e433fe
Lynn Do <linhdo@umass.edu>
2016-06-19 16:06:43 +0700

    Update 30daysofcode.java

--- 30daysofcode.java
+++ 30daysofcode.java
@@ -1,4 +1,81 @@
 import java.util.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class IO {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in); // use the Scanner class to read from stdin
+      String inputString = scan.nextLine(); // read a line of input and save it to a variable
+      scan.close(); // close the scanner
+      
+      // Your first line of output goes here
+      System.out.println("Hello, World.");
+      
+      System.out.println(inputString);
+   }
+}
+public class IO2 {
+Input Format
+The first line contains an integer, . 
+The second line contains a double, . 
+The third line contains a string, .
+
+Output Format
+Print the sum of both integers on the first line, the sum of both doubles on the second line, 
+and then the two concatenated strings on the third line.
+   
+   public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+      
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+        int i2;
+        double d2;
+        String s2;
+        
+        /* Read and save an integer, double, and String to your variables.*/
+        i2 = scan.nextInt(); // Scans the next token of the input as an int.
+        d2 = scan.nextDouble(); // Scans the next token of the input as a double.
+        scan.nextLine(); // read the rest of the line of input (newline character after the double token).
+        s2 = scan.nextLine();
+       
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + i2);
+
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d + d2);
+      
+        /* Concatenate and print the String variables on a new line integer variables on a new line; 
+           the 'string` variable above should be printed first. */
+        System.out.println(s + s2);
+        
+        scan.close();
+   }
+}
+public class Arithmetic {
+// Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent 
+// (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Calculate Tax and Tip:
+        double tip = mealCost * tipPercent / 100; // mixed double and int -- int converted to double
+        double tax = mealCost * taxPercent / 100;
+        
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost + tax + tip); // cast return-value (long) to (int)
+      
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
 public class Weird {
 // If n is odd, print Weird
 // If n is even and in the inclusive range of 2 to 5, print Not Weird
@@ -36,4 +113,26 @@ public class Person { // class: a blueprint defining the characteristics and beh
         this.age++;
 	}
 }
-
+public class Reverse {
+Sample Input
+4
+1 4 3 2
+Sample Output
+2 3 4 1
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] a = new int[len];
+        
+        // Write each element of input into an array.
+        for (int i=0; i<len; i++) {
+            a[i] = sc.nextInt();
+        }
+        // Iterate through the array in reverse, printing each element as you go.
+        for (int i=len-1; i>=0; i--) {
+            System.out.print(a[i] + " ");
+        }
+        
+        sc.close();
+    }
+}
