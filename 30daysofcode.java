6c3f92c1483d5a6e0098f0f30f42754c5b0a55a2
Lynn Do <linhdo@umass.edu>
2016-06-22 08:02:03 +0700

    Update 30daysofcode.java

--- 30daysofcode.java
+++ 30daysofcode.java
@@ -202,4 +202,37 @@ public class binaryNum {
         System.out.println(max);
     }
 }
+public class hourGlass {
+// 1 1 1 0 0 0
+// 0 1 0 0 0 0
+// 1 1 1 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
+// 0 0 0 0 0 0
 
+// a b c
+//   d
+// e f g
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        
+        int max = -63, sum; // every value in  will be in the inclusive range of -9 to 9 => min of glass = -9 * 7
+
+        for (int i=1; i < 5; i++) {
+            for (int j=1; j < 5; j++) {
+                sum = arr[i][j] + arr[i-1][j] + arr[i-1][j-1] + arr[i-1][j+1]
+                        + arr[i+1][j] + arr[i+1][j-1] + arr[i+1][j+1];
+                if (sum > max) max = sum;
+            }
+        }
+        System.out.println(max);
+            
+    }
+}
