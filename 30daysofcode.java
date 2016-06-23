0d920369290cdbc096652574f8a213f3dd6b4631
Lynn Do <linhdo@umass.edu>
2016-06-23 22:22:03 +0700

    Update 30daysofcode.java

--- 30daysofcode.java
+++ 30daysofcode.java
@@ -236,3 +236,86 @@ public class hourGlass {
             
     }
 }
+class Person {
+	
+	// instance variables
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+class Student extends Person{
+// You are given two classes, Person and Student, where Person is the base class and Student is the derived class. 
+// Observe that Student inherits all the properties of Person.
+
+// A Student class constructor, which has  parameters:
+// A string, firstName.
+// A string, lastName.
+// An integer, id.
+// An integer array (or vector) of test scores, testScores.
+
+// A char calculate() method that calculates a Student object's average and 
+// returns the grade character representative of their calculated average
+
+	private int[] testScores;
+    
+    public Student(String first, String last, int id, int[] scores) {
+        super(first, last, id); // call to superclass' constructor
+        this.testScores = scores;
+    }
+    
+    public char calculate() {
+        int avg = 0;
+        for (int score: testScores) avg += score;
+        
+        avg /= testScores.length;
+        
+        if (avg >= 90) return 'O';
+        if (avg >= 80) return 'E';
+        if (avg >= 70) return 'A';
+        if (avg >= 55) return 'P';
+        if (avg >= 40) return 'D';
+        return 'T';
+    }
+}
+class printStudent {
+// Sample Input
+// 	Heraldo Memelli 8135627
+// 	2
+// 	100 80
+// Sample Output
+//  	Name: Memelli, Heraldo
+//  	ID: 8135627
+//  	Grade: O
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate() );
+	}
+}
