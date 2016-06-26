7f7f60369782f5c7accb8fd731ca64505948f542
Lynn Do <linhdo@umass.edu>
2016-06-26 21:47:19 +0700

    Update linked-list.java

--- linked-list.java
+++ linked-list.java
@@ -9,10 +9,10 @@ class Node {
 }
 public static Node insert(Node head,int data) {
 
-		// Create a new node corresponding to data
-		Node n = new Node(data);
+	// Create a new node corresponding to data
+	Node n = new Node(data);
 	
-		// if list has no elements, return new node as head
+	// if list has no elements, return new node as head
         if (head == null) return n; 
         
         // else iterate through list, add node to tail, and return head
@@ -22,3 +22,12 @@ public static Node insert(Node head,int data) {
         return head;
     }
 
+void Print(Node head) {
+	// To print the elements of a linked list, we need to traverse the entire list and print the value of every node.
+	// The basic idea behind traversing the linked list is to follow the next pointers untill NULL is encountered. 
+	Node temp = head;
+	while (temp != null) {
+		System.out.println(temp.data);
+		temp = temp.next;
+	}
+}
